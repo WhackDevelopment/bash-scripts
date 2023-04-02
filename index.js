8a8cc761370e9ab8752a0d76c740145377410572
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2023-04-02 09:11:04 +0200

    ADD build task

--- index.js
+++ index.js
@@ -18,3 +18,91 @@
  */
 
 const fs = require('node:fs');
+const minifyBash = require('bash-minifier');
+
+if (!fs.existsSync('./dist/')) {
+    fs.mkdirSync('./dist/');
+}
+
+function isStatEqual(object1, object2) {
+    return (
+        object1.dev === object2.dev &&
+        object1.mode === object2.mode &&
+        object1.nlink === object2.nlink &&
+        object1.uid === object2.uid &&
+        object1.gid === object2.gid &&
+        object1.rdev === object2.rdev &&
+        object1.blksize === object2.blksize &&
+        object1.size === object2.size &&
+        object1.blocks === object2.blocks &&
+        object1.atimeMs === object2.atimeMs &&
+        object1.mtimeMs === object2.mtimeMs &&
+        object1.ctimeMs === object2.ctimeMs &&
+        object1.birthtimeMs === object2.birthtimeMs &&
+        object1.atime === object2.atime &&
+        object1.mtime === object2.mtime &&
+        object1.ctime === object2.ctime &&
+        object1.birthtime === object2.birthtime
+    );
+}
+
+async function recursiveWalkAndCopy(ignore = [], path, dist = './dist') {
+    console.log('Walking through' + path);
+
+    let files = fs.readdirSync(`${path}`);
+
+    for (let entity of files) {
+        let currentPath = `${path}/${entity}`;
+        let currentDest = currentPath.replace(path, dist);
+        if (ignore.includes(currentPath) || ignore.includes(currentPath + '/')) continue;
+
+        let statCurrent = fs.statSync(currentPath);
+
+        if (statCurrent.isDirectory()) {
+            recursiveWalkAndCopy(ignore, currentPath, currentDest);
+        } else {
+            try {
+                let distArgs = currentDest.split('/');
+                let filename = distArgs.pop();
+                let dir = distArgs.join('/');
+
+                fs.mkdirSync(dir, { recursive: true });
+
+                // console.log('Processing ', { currentPath, to: { dir, filename } });
+                // continue;
+
+                if (currentPath.endsWith('.sh')) {
+                    let newContent = '#!/bin/bash\n' + minifyBash(fs.readFileSync(currentPath, 'utf8'));
+                    let realDist = currentDest.replace(filename, filename.slice(0, -3));
+                    let oldContent = '';
+                    try {
+                        oldContent = fs.readFileSync(realDist, 'utf8');
+                    } catch (err) {}
+                    if (newContent === oldContent) {
+                        console.log('Skipping copy of', currentPath, 'content match in' + realDist);
+                        continue;
+                    }
+                    fs.writeFileSync(realDist, newContent, { encoding: 'utf8', flag: 'w' });
+                    console.log('Copied&Minified ', currentPath, ' to ', currentDest);
+                    continue;
+                }
+
+                if (fs.existsSync(currentDest)) {
+                    console.log('Skipping copy of', currentPath, 'file exists' + currentDest);
+                    continue;
+                }
+                // fs.createReadStream(currentPath).pipe(fs.createWriteStream(currentDist));
+                fs.copyFile(currentPath, currentDest, (err) => {
+                    if (err) throw err;
+                    console.log('Copied', currentPath, 'to', currentDest);
+                });
+            } catch (error) {
+                console.log('ERROR while Copying', currentPath, 'to', currentDest);
+
+                console.error(error);
+            }
+        }
+    }
+}
+
+recursiveWalkAndCopy(['./src/testing'], './src');
--- src/screen/minecraft/start.sh
+++ src/screen/minecraft/start.sh
@@ -19,11 +19,12 @@
 # * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # */
 
-SCREEN="$(cat .servername || echo "server-fallback-1")"
-SERVICE="FakeServer.jar"
 
+SERVICE="FakeServer.jar"
 INITMEM="1M"
 MAXMEM="8G"
+
+SCREEN="$(cat .servername || echo "server-fallback-1")"
 
 JAVA_FLAGS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
 JAVA_ARGS="-Xmx$MAXMEM -Xms$INITMEM $JAVA_FLAGS"
